c2216975f707102c02d88b1e29904567ce39e53f
Cosang <44982351+Cosang@users.noreply.github.com>
2019-01-20 22:13:04 +0200

    portofoliu

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+/* nav bar */
+
+window.onscroll = function() {stickynav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickynav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+/* end nav bar */
